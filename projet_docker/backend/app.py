f874dff246857900baa66dd05ddcc9b4328b17b5
hermannbrice12 <165166673+hermannbrice12@users.noreply.github.com>
2024-06-29 12:49:14 +0200

    Add files via upload

---
+++ projet_docker/backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from the Backend!"
+
+@app.route('/data')
+def data():
+    connection = mysql.connector.connect(
+        host='db',
+        user='user',
+        password='password',
+        database='mydatabase'
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT 'Hello, MySQL!'")
+    result = cursor.fetchone()
+    connection.close()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
